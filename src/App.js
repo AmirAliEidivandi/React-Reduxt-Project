98a0735af2d5fc9cb52e08865691c67b0dde4bfd
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-12 19:10:33 +0430

    create router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
 function App() {
-    return <div>hello world</div>;
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<ProductListing />} />
+                    <Route path="/product/:productId" element={<ProductDetail />} />
+                    <Route>404 Not Found...</Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
